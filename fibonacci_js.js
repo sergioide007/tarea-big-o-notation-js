74ee98926ab2d56dc44cfa430051c83e1d4909cc
Sergio Perez <38590176+sergioide007@users.noreply.github.com>
2018-04-28 11:33:11 -0500

    creación de js para la serie fibonacci

---
+++ fibonacci_js.js
@@ -0,0 +1,29 @@
+
+function main() {
+		var inicio = Date.now();
+    var secuencia = [];
+		for (var i = 0; i <= 45; i++) {
+			try {
+				//long fibonacci = exponential.fibonacci(i);
+				//System.out.println(i + " => " + fibonacci);
+				var fibonacci = fibonacciLineal(i, secuencia);
+				document.getElementById("fibonacci").innerHTML += "<p>"+ i + " => " + fibonacci +"<p/>";
+			} catch (e) {
+				console.log("error: "+e);
+			}
+        }
+        document.getElementById("fibonacci").innerHTML += "<p>" + "Time elapsed: " + (Date.now() - inicio)/1000 + " seconds. </p>";
+		
+}
+function fibonacciLineal(i, secuencia) {		
+		if (i == 0) {
+		    secuencia[i - 1] = 0;
+		} else {
+		    if (i - 1 <= 1) {
+			secuencia[i - 1] = 1;
+		    } else {
+			secuencia[i - 1] = secuencia[i - 2] + secuencia[i - 3];
+		    }
+		}
+		return secuencia[i - 1];			
+} 
