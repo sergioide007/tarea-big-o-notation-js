543c52f38435f936a9121a9f529b46c39e5db7b1
Sergio Perez <38590176+sergioide007@users.noreply.github.com>
2018-04-28 11:41:42 -0500

    cambio de var (variable global) a let (local)

--- fibonacci_js.js
+++ fibonacci_js.js
@@ -1,27 +1,27 @@
-
 function main() {
-		var inicio = Date.now();
-    var secuencia = [];
-		for (var i = 0; i <= 45; i++) {
-			try {
-				var fibonacci = fibonacciLineal(i, secuencia);
-				document.getElementById("fibonacci").innerHTML += "<p>"+ i + " => " + fibonacci +"<p/>";
-			} catch (e) {
-				console.log("error: "+e);
-			}
+    let inicio = Date.now();
+    let secuencia = [];
+    for (var i = 0; i <= 45; i++) {
+        try {
+            let fibonacci = fibonacciLineal(i, secuencia);
+            document.getElementById("fibonacci").innerHTML += "<p>" + i + " => " + fibonacci + "<p/>";
+        } catch (e) {
+            console.log("error: " + e);
         }
-        document.getElementById("fibonacci").innerHTML += "<p>" + "Time elapsed: " + (Date.now() - inicio)/1000 + " seconds. </p>";
-		
+    }
+    document.getElementById("fibonacci").innerHTML += "<p>" + "Time elapsed: " + (Date.now() - inicio) / 1000 + " seconds. </p>";
+
 }
-function fibonacciLineal(i, secuencia) {		
-		if (i == 0) {
-		    secuencia[i - 1] = 0;
-		} else {
-		    if (i - 1 <= 1) {
-			secuencia[i - 1] = 1;
-		    } else {
-			secuencia[i - 1] = secuencia[i - 2] + secuencia[i - 3];
-		    }
-		}
-		return secuencia[i - 1];			
-} 
+
+function fibonacciLineal(i, secuencia) {
+    if (i == 0) {
+        secuencia[i - 1] = 0;
+    } else {
+        if (i - 1 <= 1) {
+            secuencia[i - 1] = 1;
+        } else {
+            secuencia[i - 1] = secuencia[i - 2] + secuencia[i - 3];
+        }
+    }
+    return secuencia[i - 1];
+}
